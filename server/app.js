87d4e36580361264652c1d158b410b827d93c214
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-09 11:40:29 +0200

    setup passport

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const passport = require('./passport');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(routes);
 
 module.exports = app;
