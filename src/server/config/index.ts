f6793d61b6c9dbf6e2562714ed1d76a80553ee47
Marco Biedermann <marco.biedermann@gmx.de>
2019-09-10 08:53:17 +0200

    refactor: config
    Refactor config type definition

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -3,11 +3,30 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
-  [key: string]: any;
+  port: number;
+  session: {
+    secret: string;
+  };
+  mongodbUri: string;
+  github: {
+    clientId: string;
+    clientSecret: string;
+    callbackUrl: string;
+  };
+  google: {
+    clientId: string;
+    clientSecret: string;
+    callbackUrl: string;
+  };
+  twitter: {
+    consumerKey: string;
+    consumerSecret: string;
+    callbackUrl: string;
+  };
 }
 
 const config: IConfig = {
-  port: process.env.PORT,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
   session: {
     secret: process.env.SESSION_SECRET,
   },
