3b6f1d5e3f4b377e2aa4be4eb33786a1ee252dbe
Marco Biedermann <marco.biedermann@gmx.de>
2020-07-03 09:00:25 +0200

    feat: Helmet
    Add Helmet to secure app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import { errors } from 'celebrate';
 import express from 'express';
 import expressSession from 'express-session';
+import helmet from 'helmet';
 import handleError from './middlewares/error';
 import passport from './passport';
 import routes from './routes';
@@ -17,6 +18,7 @@ app.use(
     secret: 'keyboard cat',
   }),
 );
+app.use(helmet());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
