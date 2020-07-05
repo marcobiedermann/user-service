659060baafefc7ed2cc86edfc735b14c2c2df991
Marco Biedermann <marco.biedermann@gmx.de>
2020-07-05 14:32:03 +0200

    feat: me endpoint
    Add me endpoint to get current user

---
+++ src/routes/me/index.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import passport from 'passport';
+
+const router = Router();
+
+async function getMe(request: Request, response: Response) {
+  const { user } = request;
+
+  response.json({
+    user,
+  });
+}
+
+router.route('/me').get(passport.authenticate('basic'), asyncHandler(getMe));
+
+export default router;
