9ff309e6435d2914b6fcda41a1107fd38c065e84
Marco Biedermann <marco.biedermann@gmx.de>
2020-07-06 21:08:05 +0200

    fix: Google scope
    Add mail to scope

--- src/routes/auth/google/index.ts
+++ src/routes/auth/google/index.ts
@@ -3,7 +3,11 @@ import passport from '../../../passport';
 
 const router = Router();
 
-router.route('/google').get(passport.authenticate('google'));
+router.route('/google').get(
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  }),
+);
 
 router.route('/google/callback').get(
   passport.authenticate('google', {
