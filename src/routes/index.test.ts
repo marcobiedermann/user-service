45144c1c2fd3376ecc7a37b7cd95ad0a8dfb0c64
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2021-10-31 16:53:53 +0100

    perf: Tests
    Improve runtime of tests

--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -1,14 +1,12 @@
 import supertest from 'supertest';
 import app from '../app';
 
-const request = supertest(app);
-
 describe('routes/api', () => {
   describe('get /', () => {
     it('should redirect to `/api-docs`', async () => {
       expect.assertions(2);
 
-      const response = await request.get('/');
+      const response = await supertest(app).get('/');
 
       expect(response.status).toStrictEqual(302);
       expect(response.header.location).toStrictEqual('/api-docs');
